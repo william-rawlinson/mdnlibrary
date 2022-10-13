4ce3ac31c4e9a30de2777fde8cd7531ae1af8328
Will <william.rawlinson1@hotmail.com>
2022-10-13 20:46:45 +0100

    Explore templates

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -11,12 +11,16 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_genres_fiction = Genre.objects.filter(name__contains = 'fiction').count()
+    num_books_the = Book.objects.filter(title__contains = 'the').count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres_fiction': num_genres_fiction,
+        'num_books_the': num_books_the,
     }
 
     return render(request, 'index.html', context=context)
